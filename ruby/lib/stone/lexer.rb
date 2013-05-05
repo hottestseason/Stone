bcbe4d103db17f93785e57cfe387cc018659068e
Sumihiko Natsu <hottestseason@gmail.com>
2013-05-05 16:41:02 +0900

    Move ruby src to new directory

--- ruby/lib/stone/lexer.rb
+++ ruby/lib/stone/lexer.rb
@@ -18,16 +18,16 @@ module Stone
       io.each_line.with_index do |line, i|
         line.scan(REGEXP) do |matched, comment, number_literal, id_literal, string_literal|
           if number_literal
-            tokens << NumberToken.new(i, number_literal)
+            @tokens << NumberToken.new(i, number_literal)
           end
           if id_literal
-            tokens << IdToken.new(i, id_literal)
+            @tokens << IdToken.new(i, id_literal)
           end
           if string_literal
-            tokens << StringToken.new(i, string_literal)
+            @tokens << StringToken.new(i, string_literal)
           end
         end
-        tokens << IdToken.new(i, "\n")
+        @tokens << IdToken.new(i, "\n")
       end
     end
   end
